d758b59663376f30cd45b27711b0cfc55177a37e
Ampflower <ampflower@outlook.com>
2023-02-21 11:08:22 -0600

    Support attachments & clean up Main

--- src/main/java/io/github/debuggyteam/modmail/Main.java
+++ src/main/java/io/github/debuggyteam/modmail/Main.java
@@ -3,29 +3,47 @@ package io.github.debuggyteam.modmail;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.channel.ChannelType;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import java.util.Objects;
 
 /**
  * @author Ampflower
  * @since ${version}
  **/
 public final class Main {
-	//private static final String token = System.getenv("__DEBUGGY_MODMAIL_TOKEN");
+	private static final String token = System.getenv("__DEBUGGY_MODMAIL_TOKEN");
+	private static final String channel = System.getenv("__DEBUGGY_MODMAIL_TARGET");
 
 	public static JDA client;
+	public static GuildMessageChannel targetChannel;
 
-	public static void main(String[] args) {
-		//TODO: Don't include the token
-		final var builder = JDABuilder.createLight("bleh")
+	static {
+		Objects.requireNonNull(token, "Set environment __DEBUGGY_MODMAIL_TOKEN to your bot's token.");
+		Objects.requireNonNull(channel, "Set environment __DEBUGGY_MODMAIL_TARGET to your chosen channel.");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		final var builder = JDABuilder.createLight(token)
 			.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
 			.setActivity(Activity.of(Activity.ActivityType.WATCHING, "for your DMs!"))
 			// Even though createLight should already disallow these, I'm putting these here anyways.
 			.disableCache(CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS, CacheFlag.ONLINE_STATUS, CacheFlag.SCHEDULED_EVENTS)
-				.addEventListeners(new SomethingAboutMessageEvents());
-		
-		client = builder.build();
+			.addEventListeners(new SomethingAboutMessageEvents());
+
+		client = builder.build().awaitReady();
+
+		targetChannel = client.getTextChannelById(channel);
+
+		if (targetChannel == null) {
+			System.err.println("""
+				Set environment __DEBUGGY_MODMAIL_TARGET to a valid channel.
+
+				It is currently set to the following:\s""" + channel);
+
+			System.exit(1);
+		}
 	}
 }
--- src/main/java/io/github/debuggyteam/modmail/SomethingAboutMessageEvents.java
+++ src/main/java/io/github/debuggyteam/modmail/SomethingAboutMessageEvents.java
@@ -2,16 +2,14 @@ package io.github.debuggyteam.modmail;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * @author woodiertexas
@@ -22,39 +20,84 @@ public class SomethingAboutMessageEvents implements EventListener {
     @Override
     public void onEvent(@NotNull GenericEvent event) {
         EmbedBuilder theEmbed = new EmbedBuilder();
-        long debuggyBotTest = 1077094694485512273L;
-        
+
         if (event instanceof MessageReceivedEvent msgEvent) {
             if (msgEvent.isFromType(ChannelType.PRIVATE)) {
-                var theMessage = msgEvent.getMessage();
-                var theUser = msgEvent.getMessage().getAuthor();
-                List<Message.Attachment> theAttachments = theMessage.getAttachments();
-                
-                theEmbed.setAuthor(theUser.getName(), theUser.getAvatarUrl(), theUser.getAvatarUrl());
-                theEmbed.setFooter(theUser.getId() + " • Community Manager");
-                theEmbed.setDescription(theMessage.getContentRaw());
-                
-                int size = theAttachments.size();
-                
-                if (theAttachments.isEmpty()) {
-                    Objects.requireNonNull(Main.client.getTextChannelById(debuggyBotTest)).sendMessageEmbeds(theEmbed.build()).queue();
-                } else if (size == 1) {
-                    theEmbed.setImage(theAttachments.get(0).getUrl());
-                    Objects.requireNonNull(Main.client.getTextChannelById(debuggyBotTest)).sendMessageEmbeds(theEmbed.build()).queue();
-                } else if (size >= 2) {
+				var theMessage = msgEvent.getMessage();
+				var theUser = msgEvent.getMessage().getAuthor();
+				List<Message.Attachment> theAttachments = theMessage.getAttachments();
 
-                    var channel = Main.client.getTextChannelById(debuggyBotTest);
-                    var builder = channel.sendMessageEmbeds(theEmbed.build());
-                    
-                    for (int i = 0; i < size; i++) {
-                        theEmbed.setImage(theAttachments.get(i).getUrl());
-                        
-                        builder.addEmbeds(theEmbed.build());
-                    }
-                    
-                    builder.queue();
-                }
-            }
-        }
-    }
+				boolean spoilEmbeds = false;
+				final var attachmentList = new ArrayList<String>(theAttachments.size());
+
+				theEmbed.setAuthor(theUser.getName(), theUser.getAvatarUrl(), theUser.getAvatarUrl());
+				theEmbed.setFooter(theUser.getId() + " • Community Manager");
+				theEmbed.setDescription(theMessage.getContentRaw());
+
+				final var itr = theAttachments.iterator();
+
+				// Finds first image to embed.
+				while (itr.hasNext()) {
+					final var attachment = itr.next();
+
+					if (attachment.isImage()) {
+						// This only is needed for images.
+						// Everything else can be spoiled via pipes.
+						spoilEmbeds = attachment.isSpoiler();
+
+						theEmbed.setImage(attachment.getUrl());
+
+						// No need to continue here; we found the first image.
+						break;
+					} else {
+						appendAttachment(attachmentList, attachment);
+					}
+				}
+
+				var builder = Main.targetChannel.sendMessageEmbeds(theEmbed.build());
+
+				// Reuse builder
+				theEmbed.clear();
+
+				while (itr.hasNext()) {
+					final var attachment = itr.next();
+
+					if (attachment.isImage()) {
+						spoilEmbeds |= attachment.isSpoiler();
+						theEmbed.setImage(attachment.getUrl());
+						builder.addEmbeds(theEmbed.build());
+					} else {
+						appendAttachment(attachmentList, attachment);
+					}
+				}
+
+
+				if (!attachmentList.isEmpty()) {
+					theEmbed.clear();
+					theEmbed.setTitle("Additional attachments");
+
+					final var descriptionBuilder = theEmbed.getDescriptionBuilder();
+					for (final var attachment : attachmentList) {
+						descriptionBuilder.append(attachment).append('\n');
+					}
+
+					builder.addEmbeds(theEmbed.build());
+				}
+
+				if (spoilEmbeds) {
+					builder.setContent("The message sent by ``" + theUser.getName() + "`` had attachments as spoilers. ||http://./||");
+				}
+
+				builder.queue();
+			}
+		}
+	}
+
+	private static void appendAttachment(List<String> attachmentList, Message.Attachment attachment) {
+		if (attachment.isSpoiler()) {
+			attachmentList.add("||" + attachment.getUrl() + "||");
+		} else {
+			attachmentList.add(attachment.getUrl());
+		}
+	}
 }

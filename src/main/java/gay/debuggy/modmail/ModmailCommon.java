468493ca728a1e3692c39b69de1ffa116330612d
woodiertexas <woodietexas@protonmail.com>
2023-05-04 19:04:10 -0700

    move utility methods to ModmailCommon.java

---
+++ src/main/java/gay/debuggy/modmail/ModmailCommon.java
@@ -0,0 +1,40 @@
+package gay.debuggy.modmail;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+
+public class ModmailCommon {
+	/**
+	 * Checks if a url ends with an executable extension.
+	 *
+	 * @param url The URL to check.
+	 * @return boolean
+	 */
+	static boolean isExecutable(String url) {
+		String[] listOfExtensions = {
+			".sh", ".exe", ".scr", ".bat", ".vbs",
+			".cmd", ".msi", ".com", ".efi", ".o"
+		};
+
+		for (String extension : listOfExtensions) {
+			if (url.endsWith(extension)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Creates a common embed.
+	 *
+	 * @param embed         The EmbedBuilder to use.
+	 * @param authorName    The author's name (can be a user, a guild, or something else).
+	 * @param authorIconUrl The author's icon URL (can be a user, a guild, or something else).
+	 * @param description   The text description.
+	 * @param color         The embed's bar color.
+	 */
+	static void createCommonEmbed(EmbedBuilder embed, String authorName, String authorIconUrl, String description, int color) {
+		embed.setAuthor(authorName, authorIconUrl, authorIconUrl);
+		embed.setDescription(description);
+		embed.setColor(color);
+	}
+}
--- src/main/java/gay/debuggy/modmail/SomethingAboutMessageEvents.java
+++ src/main/java/gay/debuggy/modmail/SomethingAboutMessageEvents.java
@@ -52,7 +52,7 @@ public class SomethingAboutMessageEvents extends ListenerAdapter {
 
 			for (int i = 0; i < theMessage.getAttachments().size(); i++) {
 
-				if (isExecutable(theMessage.getAttachments().get(i).getUrl())) {
+				if (ModmailCommon.isExecutable(theMessage.getAttachments().get(i).getUrl())) {
 					executableFound = true;
 					executableUrls.add(theMessage.getAttachments().get(i).getUrl());
 				}
@@ -65,7 +65,7 @@ public class SomethingAboutMessageEvents extends ListenerAdapter {
 				//handleMessage(msgEvent, targetChannel);
 
 				if (!doesThreadExist) {
-					createCommonEmbed(theEmbed, theUser.getAsTag(), theUser.getAvatarUrl(), theUser.getAsMention() + " registered their account <t:" + theUser.getTimeCreated().toEpochSecond() + ":R>", 0x000000);
+					ModmailCommon.createCommonEmbed(theEmbed, theUser.getAsTag(), theUser.getAvatarUrl(), theUser.getAsMention() + " registered their account <t:" + theUser.getTimeCreated().toEpochSecond() + ":R>", 0x000000);
 					theEmbed.addField("Registered @", "<t:" + theUser.getTimeCreated().toEpochSecond() + ">", true);
 					theEmbed.addField("User ID", theUser.getId(), true);
 
@@ -82,12 +82,12 @@ public class SomethingAboutMessageEvents extends ListenerAdapter {
 					handleMessage(msgEvent, client.getThreadChannelById(modmailThread.get(theUser.getIdLong())));
 				}
 			} else {
-				createCommonEmbed(theEmbed, theUser.getName(), theUser.getAvatarUrl(), theMessage.getContentRaw(), 0xFF0000);
+				ModmailCommon.createCommonEmbed(theEmbed, theUser.getName(), theUser.getAvatarUrl(), theMessage.getContentRaw(), 0xFF0000);
 				theEmbed.addField("Executable link(s) found:", String.valueOf(executableUrls), false);
 				targetChannel.sendMessageEmbeds(theEmbed.build()).queue();
 
 				theEmbed.clear();
-				createCommonEmbed(theEmbed, guild.getName(), guild.getIconUrl(), "Your latest message contains one or more executable files. Please do not send executables in modmail.", 0xFF0000);
+				ModmailCommon.createCommonEmbed(theEmbed, guild.getName(), guild.getIconUrl(), "Your latest message contains one or more executable files. Please do not send executables in modmail.", 0xFF0000);
 				theMessage.getChannel().sendMessageEmbeds(theEmbed.build()).queue();
 
 			}
@@ -198,40 +198,5 @@ public class SomethingAboutMessageEvents extends ListenerAdapter {
 		} else {
 			attachmentList.add(attachment.getUrl());
 		}
-	}
-
-	/**
-	 * Checks if a url ends with an executable extension.
-	 *
-	 * @param url The URL to check.
-	 * @return boolean
-	 */
-	boolean isExecutable(String url) {
-		String[] listOfExtensions = {
-			".sh", ".exe", ".scr", ".bat", ".vbs",
-			".cmd", ".msi", ".com", ".efi", ".o"
-		};
-
-		for (String extension : listOfExtensions) {
-			if (url.endsWith(extension)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * Creates a common embed.
-	 *
-	 * @param embed         The EmbedBuilder to use.
-	 * @param authorName    The author's name (can be a user, a guild, or something else).
-	 * @param authorIconUrl The author's icon URL (can be a user, a guild, or something else).
-	 * @param description   The text description.
-	 * @param color         The embed's bar color.
-	 */
-	void createCommonEmbed(EmbedBuilder embed, String authorName, String authorIconUrl, String description, int color) {
-		embed.setAuthor(authorName, authorIconUrl, authorIconUrl);
-		embed.setDescription(description);
-		embed.setColor(color);
 	}
 }
